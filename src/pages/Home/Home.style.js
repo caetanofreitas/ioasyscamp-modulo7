21e465edd741dc3235eef843d82c36610403d5c9
Caetano <caetanofreitas@ioasys.com.br>
2022-02-08 08:11:25 -0300

    feat: adding cards container

--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -7,3 +7,39 @@ export const Container = styled.div`
   width: 100%;
   min-height: 100vh;
 `;
+
+export const Content = styled.main`
+  display: flex;
+  margin: 0 auto;
+  padding: 0 16px;
+  padding-top: 40px;
+  max-width: 1135px;
+  flex-direction: column;
+
+  @media (min-width: 1135px) {
+    padding: 0;
+    padding-top: 40px;
+  }
+`
+
+export const CardsContainer = styled.div`
+  gap: 16px;
+  display: grid;
+  margin-top: 40px;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  grid-template-columns: 1fr;
+
+  @media (min-width: 602px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (min-width: 902px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  @media (min-width: 1135px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+`
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,20 @@
 import Header from '../../components/Header';
+import Card from '../../components/Card';
 import * as S from './Home.style';
+
+import books from '../../mocks/books.json';
 
 const Home = () => {
   return (
     <S.Container>
       <Header mode="dark" />
+      <S.Content>
+        <S.CardsContainer>
+          {books.map((book) => (
+            <Card key={book.id} book={book} />
+          ))}
+        </S.CardsContainer>
+      </S.Content>
     </S.Container>
   );
 }
